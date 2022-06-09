61d89a38bf7f1003884f7ff1b4740bd65edbb84a
Guillaume Lebigot <axel@teri-chan.net>
2022-06-09 08:45:28 +0200

    Fixed ES Module config for typescript and moved CLI to its own file

---
+++ src/cli.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import * as fs from 'fs/promises';
+import { convertToASS } from './index.js';
+
+async function mainCLI() {
+	if (!process.argv[2]) {
+		throw `srt2ass - Convert SRT to ASS file
+		Usage: srt2ass myfile.srt
+		Output goes to stdout
+		`;
+	}
+	const txtFile = process.argv[2];
+	if (!await fs.stat(txtFile)) throw `File ${txtFile} does not exist`;
+	const txt = await fs.readFile(txtFile, 'utf8');
+	return convertToASS(txt);
+}
+
+mainCLI()
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-#!/usr/bin/env node
-import { AssToMs, clone, convertSRTTags, msToAss } from './utils.cjs';
+import { AssToMs, clone, convertSRTTags, msToAss } from './utils.js';
 import stringify from 'ass-stringify';
-import * as ass from './assTemplate.cjs';
-import * as fs from 'fs/promises';
+import * as ass from './assTemplate.js';
 
 function generateASSLine(line: any, styles: any) {
 	let startMs = +line.startTime - 1000;
 	if (startMs < 0) startMs = 0;
-	const stopMs = +line.endTime + 100;	
+	const stopMs = +line.endTime + 100;
 	const dialogue = clone(ass.dialogue);
 	const comment = clone(ass.dialogue);
 	dialogue.value.Start = comment.value.Start = msToAss(startMs);
@@ -43,21 +41,20 @@ export function parseSRT(srt: string): {startTime: string, endTime: string, text
 	let insideSubSegment = false;
 	let sub = [];
 	for (const line of rawArr) {
-		console.log(line);
 		// First line should always be a number.
 		if (+line > 0 && !insideSubSegment) {
 			insideSubSegment = true;
 			continue;
 		}
-		if (line.match(/\d+:\d\d:\d\d,\d\d\d --> \d+:\d\d:\d\d,\d\d\d/)) {			
+		if (line.match(/\d+:\d\d:\d\d,\d\d\d --> \d+:\d\d:\d\d,\d\d\d/)) {
 			const startTime = line.split(' --> ')[0].replaceAll(',','.');
 			const endTime = line.split(' --> ')[1].replaceAll(',','.');
 			// Let's convert those to miliseconds because we'll need to add the 1s delay so lines appear a little earlier than expected.
 			subSegment.startTime = AssToMs(startTime);
 			subSegment.endTime = AssToMs(endTime);
-			continue;			
+			continue;
 		}
-		if (line === '') {			
+		if (line === '') {
 			// Sub segment is done
 			subSegment.text = sub.join('\N');
 			ass.push(subSegment);
@@ -70,7 +67,7 @@ export function parseSRT(srt: string): {startTime: string, endTime: string, text
 			sub = [];
 			continue;
 		}
-		// Last possibility is that this contains text. So we're adding it.		
+		// Last possibility is that this contains text. So we're adding it.
 		sub.push(convertSRTTags(line)
 		);
 	}
@@ -86,7 +83,7 @@ export function convertToASS(text: string): string {
 	const sub = parseSRT(text);
 	const dialogues = [];
 	const comments = [];
-	const styles = clone(ass.styles);	
+	const styles = clone(ass.styles);
 	const script = clone(ass.dialogue);
 	script.value.Effect = ass.scriptFX;
 	script.value.Text = ass.script;
@@ -96,27 +93,10 @@ export function convertToASS(text: string): string {
 		const ASSLines = generateASSLine(line, styles);
 		comments.push(clone(ASSLines.comment));
 		dialogues.push(clone(ASSLines.dialogue));
-	}	
+	}
 	comments.sort(sortStartTime);
 	dialogues.sort(sortStartTime);
 	const events = clone(ass.events);
 	events.body = events.body.concat(comments, dialogues);
 	return stringify([ass.scriptInfo, styles, events]);
 }
-
-async function mainCLI() {
-	if (!process.argv[2]) {
-		throw `srt2ass - Convert SRT to ASS file
-		Usage: srt2ass myfile.srt
-		Output goes to stdout
-		`;
-	}
-	const txtFile = process.argv[2];
-	if (!await fs.stat(txtFile)) throw `File ${txtFile} does not exist`;
-	const txt = await fs.readFile(txtFile, 'utf8');
-	return convertToASS(txt);
-}
-
-if (require.main === module) mainCLI()
-	.then(data => console.log(data))
-	.catch(err => console.log(err));
